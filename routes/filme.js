0c4c8b73c4d4c6b99d6dd2ba7d34104e04a4a3ef
jorgeigor <jorgeigor.1995@gmail.com>
2019-12-05 14:30:48 -0300

    deploy heroku

--- routes/filme.js
+++ routes/filme.js
@@ -3,7 +3,7 @@ module.exports = function (app) {
     var usuario = app.controllers.usuario;
    // app.get('/filme', filme.index);
        app.post('/filme/create',  filme.create);
-       app.get('/usuario/filme/edit/:id', filme.edit );
+       app.get('/usuario/filme/edit/:id', filme.edit);
        app.post('/usuario/filme/:id', filme.validate, filme.update);
        app.get('/usuario/filme/:id', filme.destroy);
 
